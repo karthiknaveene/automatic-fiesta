e05a00a9ff4bbd5d3082f44863d7a7d0ac289069
karthiknaveene <kelango@cloudbees.com>
2026-08-21 11:00:48 +0530

    Testing 2

--- main.go
+++ main.go
@@ -1,30 +1,34 @@
- package main
+package main
 
-  import (
-        "fmt"
-        "math/rand"
-        "net/http"
-        "os/exec"
-  )
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os/exec"
+)
 
-  // G101: hardcoded credentials
-  const password = "supersecret123"
-  const apiKey = "hardcoded-api-key-abc123"
+// G101: hardcoded credentials
+const password = "supersecret123"
+const apiKey = "hardcoded-api-key-abc123"
 
-  func main() {
-        // G404: weak random number
-        fmt.Println(rand.Int())
+func main() {
+	// G404: weak random number
+	fmt.Println(rand.Int())
 
-        // G204: subprocess launched with variable
-        userInput := "ls"
-        cmd := exec.Command(userInput)
-        cmd.Run()
+	// G204: subprocess launched with variable
+	userInput := "ls"
+	cmd := exec.Command(userInput)
+	cmd.Run()
 
-        // G107: url provided to HTTP request as taint input
-        url := "http://example.com"
-        http.Get(url)
+	// G107: url provided to HTTP request as taint input
+	url := "http://example.com"
+	http.Get(url) //nolint
 
-        // G501: import of weak crypto
-        fmt.Println(password, apiKey)
-  }
+	// G501: import of weak crypto
+	fmt.Println(password, apiKey)
 
+	// G115: integer overflow (CWE-190)
+	var i int32 = 2147483647
+	var j uint32 = uint32(i)
+	fmt.Println(j)
+}
